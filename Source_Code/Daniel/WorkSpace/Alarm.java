1b6ac4c26811b1ba010f93bdb35a90ec9b1be061
dcoroneljr <dcoroneljr@miners.utep.edu>
2016-03-16 22:14:04 -0600

    Added files via upload

---
+++ Source_Code/Daniel/WorkSpace/Alarm.java
@@ -0,0 +1,8 @@
+
+public class Alarm {
+	/*
+	 *Methods: 
+	 * alertOfDeadline
+	 * alertofModifications
+	 */
+}
---
+++ Source_Code/Daniel/WorkSpace/Log.java
@@ -0,0 +1,11 @@
+
+public class Log {
+	/*
+	 *Attributes:
+	 * user
+	 * change
+	 * dateOfChange 
+	 *Methods: 
+	 * trackModifications
+	 */
+}
